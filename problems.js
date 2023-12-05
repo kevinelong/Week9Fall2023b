8d2bce208953c34f069bbb3110a44c46a7a95eab
kevinelong <kevinelong@gmail.com>
2023-12-05 09:30:49 -0800

    problems

---
+++ problems.js
@@ -0,0 +1,32 @@
+/***
+ * Given a set of coins, how can caculate the total?
+ */
+
+function total(coins) {
+    //YOUR CODE HERE
+    return
+}
+console.log(total([1, 1, 5, 10, 25, 25]) == 67)
+
+/***
+ * What is value of this collection of coins? Expressed as denomination:quantity in an object
+ */
+function getValue(dictObject){
+    //TODO 
+    
+}
+console.log(67 === getValue({
+    25: 2,
+    10: 1,
+    5: 1,
+    1: 2
+}));
+
+/***
+ * What is the minumum set of coins to make change
+ */
+function makeChange(amount) {
+    //TODO WRITE CODE HERE
+    return;
+}
+console.log(makeChange(amount) == JSON.stringify([25,25,10,5,1,1]));
